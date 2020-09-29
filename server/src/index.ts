e97fa6881df35cb8da19dda92698f1e4a30cfbff
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-09-29 08:16:59 +0530

    added migrations + find posts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,19 @@ import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
+  // Connect to database
   const orm = await MikroORM.init(mikroConfig);
 
-  const post = orm.em.create(Post, { title: "First Post" });
-  await orm.em.persistAndFlush(post);
-  console.log("---------sql2---------");
-  await orm.em.nativeInsert(Post, { title: "First Post" });
+  //   Get migrations
+  await orm.getMigrator().up();
+
+  //   Insert post into database
+  //   const post = orm.em.create(Post, { title: "First Post" });
+  //   await orm.em.persistAndFlush(post);
+
+  //   Find posts
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
 main().catch((err) => console.error(err));
