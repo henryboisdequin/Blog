744cd2dd0126e7a2a61d300da8b8b0b1f182e638
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-10-01 16:26:10 +0530

    connect to backend + codegen init

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,3 @@
-// Warning
-console.info(
-  `
-%c🛑 STOP 🛑
-This is a browser feature intended for developers. If you are not a developer, kindly exit this page.`,
-  `font-size: 34px; color: red; font-family: "Trebuchet MS", Helvetica, sans-serif;`
-);
-
 const Index = () => (
   <div>
     <h1 style={{ fontSize: "34px" }}>Hello World</h1>
