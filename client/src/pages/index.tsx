726b5cd240c90602ec90fe756fb17d814d22a055
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-10-02 10:22:34 +0530

    cleanup + setup for server side rendering

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <div>
-    <NavBar />
-    <h1>Hello World</h1>
-  </div>
-);
+const Index = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Hello World</h1>
+    </div>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
