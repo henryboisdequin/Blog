cdc71a99eff6acecd979c13f76bec82d7d0fbadf
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-10-10 14:41:59 +0530

    writing fragments to update cache

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -13,8 +13,10 @@ import {
   MeDocument,
   MeQuery,
   RegisterMutation,
+  VoteMutationVariables,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
+import gql from "graphql-tag";
 
 const errorExchange: Exchange = ({ forward }) => (ops$) => {
   return pipe(
@@ -138,6 +140,29 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables;
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  id
+                  points
+                }
+              `,
+              { id: postId } as any
+            );
+            if (data) {
+              const newPoints = (data.points as number) + value;
+              cache.writeFragment(
+                gql`
+                  fragment __ on Post {
+                    points
+                  }
+                `,
+                { id: postId, points: newPoints } as any
+              );
+            }
+          },
           createPost: (_result, args, cache, info) => {
             const allFields = cache.inspectFields("Query");
             const fieldInfos = allFields.filter(
